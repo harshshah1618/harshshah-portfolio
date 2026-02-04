18553851a8ee032c298665b117dabdd7edb8576a
harshshah1618 <harshshah1618@users.noreply.github.com>
2026-02-04 02:56:49 +0000

    [ci] deploy site 40847ff937035a7605d9ea3d49093692eb7d35db

---
+++ assets/use-pagetitle-1sK612Mf.js
@@ -0,0 +1,1 @@
+import{r as t,E as s}from"./index-B9rwK-U8.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
--- assets/use-pagetitle-CuEp2wqE.js
+++
@@ -1,1 +0,0 @@
-import{r as t,E as s}from"./index-BwTgahUV.js";function o(e){t.useEffect(()=>{document.title=`${e} - ${s.name}`},[e])}export{o as u};
